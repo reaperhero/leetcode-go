889b02cd26835dc368112655eb151708404c44c9
chenqiangjun <unityboby@gmail.com>
2020-09-30 12:51:38 +0800

    chenqiangjun update

--- go-datastructures/binaryheap/binaryheap_01_test.go
+++ go-datastructures/binaryheap/binaryheap_01_test.go
@@ -7,12 +7,11 @@ import (
 	"testing"
 )
 
-
-
 // 二叉堆（英语：binary heap）是一种特殊的堆，二叉堆是完全二叉树或者是近似完全二叉树
+//
 func Test_binaryheap(t *testing.T) {
 
-	// Min-heap
+	// 最小堆
 	heap := binaryheap.NewWithIntComparator() // empty (min-heap)
 	heap.Push(2)
 	heap.Push(3)
@@ -22,20 +21,24 @@ func Test_binaryheap(t *testing.T) {
 	heap.Push(6)
 	heap.Push(6)
 	heap.Push(2)
-	fmt.Println(heap.Values())                // [1 2 2 3 5 6 6 4]
-	_, _ = heap.Peek()
-	// 1,true
-	_, _ = heap.Pop()                         // 1, true
-	_, _ = heap.Pop()                         // 2, true
-	_, _ = heap.Pop()                         // 2, true
-	// ...
-	_, _ = heap.Pop()                         // nil, false (nothing to pop)
-	heap.Push(1)                      // 1
-	heap.Clear()                              // empty
-	heap.Empty()                              // true
-	heap.Size()                               // 0
+	fmt.Println(heap.Values()) // [1 2 2 3 5 6 6 4]
+	//   1
+	//  2   2
+	// 3 5 6 6
+	//4
+	_, _ = heap.Peek() // 1,true
 
-	// Max-heap
+	_, _ = heap.Pop() // 1, true
+	_, _ = heap.Pop() // 2, true
+	_, _ = heap.Pop() // 2, true
+	// ...
+	_, _ = heap.Pop() // nil, false (nothing to pop)
+	heap.Push(1)  // 1
+	heap.Clear()      // empty
+	heap.Empty()      // true
+	heap.Size()       // 0
+
+	// 最大堆
 	inverseIntComparator := func(a, b interface{}) int {
 		return -utils.IntComparator(a, b)
 	}
@@ -49,5 +52,9 @@ func Test_binaryheap(t *testing.T) {
 	heap.Push(6)
 	heap.Push(2)
 	fmt.Println(heap.Values()) // [6 4 6 2 3 1 5 2]
-	heap.Values()                                   // 3, 2, 1
+	//   	6
+	// 	4    6
+	// 2 3  1 5
+	//2
+	heap.Values()
 }
